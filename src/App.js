370568f257dd3dce4082ed7a75276098cae69358
RafiTahmid <rafi.tahmid019@gmail.com>
2022-09-23 12:00:06 +0600

    Event handler added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <District></District>
+      <District name="Bhola" special="Doi"></District>
+      <District name="B.baria" special="Fighter"></District>
+      <District name="Barisal" special="Monu"></District>
     </div>
   );
 }
 
-function District(){
+function District(props){
+  const [power, setPower] = useState(1)
+  const boostPower = () =>{
+    const newPower = power * 2
+    setPower(newPower);
+  }
   return(
-    <div>
-      <h2>Name: </h2>
-      <p>Speciality:</p>
+    <div className='district'>
+      <h2>Name: {props.name}</h2>
+      <p>Specialty: {props.special}</p>
+      <h4>Power: {power}</h4>
+      <button onClick={boostPower}>Boost The Power</button>
     </div>
   )
 }
