4111b4ed738e4fe378526e08d52550986d8c0b91
RafiTahmid <rafi.tahmid019@gmail.com>
2022-09-23 12:39:59 +0600

    dynamically loaded data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <LoadPosts></LoadPosts>
       <District name="Bhola" special="Doi"></District>
       <District name="B.baria" special="Fighter"></District>
       <District name="Barisal" special="Monu"></District>
@@ -12,7 +13,41 @@ function App() {
   );
 }
 
+function LoadPosts(){
+  const [posts, setPosts] = useState([])
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setPosts(data))
+  }, [])
+  return(
+    <div>
+      <h1>Posts: {posts.length}</h1>
+      {
+        posts.map(post => <Post title={post.title} body={post.body}></Post>)
+      }
+    </div>
+  )
+}
+
+const componentName = {
+  backgroundColor : 'lightblue',
+  margin: '20px',
+  padding: '20px',
+  border: '2px solid purple',
+  borderRadius: '20px'
+}
+function Post(props) {
+  return(
+    <div style={componentName}>
+      <h2>Title: {props.title}</h2>
+      <p>Body: {props.body}</p>
+    </div>
+  )
+}
+
 function District(props){
+  // Declaring State
   const [power, setPower] = useState(1)
   const boostPower = () =>{
     const newPower = power * 2
